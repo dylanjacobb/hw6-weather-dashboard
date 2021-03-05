d31b6cd305420d242b244f2967fbb34c6de01549
Dylan Jacob <dylanjacob.re@gmail.com>
2021-03-05 09:04:05 -0500

    added weather data

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,152 @@
 var appKey = '5d0479bcef657d162bba17e27204ff00'
-// for the search input
-// var city = document.querySelector('input-field');
-// add var for the input fields so I can add that to the cityURL variable
-var city = 'charlotte'
-var cityName = document.querySelector('.city-name');
+var city = document.querySelector('input-field');
+// var city = document.querySelector('input');
+// var city = 'charlotte';
 var unit = 'imperial';
-// link to the weather thing
-var cityURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=' + unit;
-
+var mainWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=' + unit;
+var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + appKey + '&units=' + unit;
 var searchButton = document.querySelector('#button-addon2');
-// for the displayed date
-var date = moment(); $(".displayed-date").text(date.format("MMM Do, YYYY"));
-date.textContent = (date);
-console.log(date);
-console.log(cityURL);
+var mainWeatherDate = moment().format('MM/DD/YY');
 
-// forcast url
-var fiveDayForcast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + appKey;
-console.log(fiveDayForcast);
-
-// adds an even listener to the search button so that when its 'clicked' it'll fetch the data from the weather api link.
-searchButton.addEventListener('click', function () {
-    fetch(cityURL)
+// gets the main weaether data shown
+function mainWeatherData() {
+    fetch(mainWeatherURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            // this var here is used to dispaly the temperature in text form on the html
-            var temperatureLi = document.querySelector('.temperature-li');
-            console.log(temperatureLi);
-            temperatureLi.textContent = parseInt(data.main.temp);
+            var titleEl = document.querySelector('.city-name');
+            var temperatureEl = document.querySelector('.temperature-li');
+            var humidityEl = document.querySelector('.humidity-li');
+            var windSpeedEl = document.querySelector('.wind-speed-li');
+            var citySelector = document.querySelector('.city-name');
+            // var uvIndexEl = document.querySelector('.uv-index-li');
+            citySelector.textContent = (data.name);
+            temperatureEl.textContent = ('Temperature: ' + data.main.temp + '°F');
+            humidityEl.textContent = ('Humidity: ' + data.main.humidity + '%');
+            windSpeedEl.textContent = ('Wind Speed: ' + data.wind.speed + ' MPH');
+            titleEl.textContent = (data.name + ' (' + mainWeatherDate + ')');
+            // TODO: pull another api for the uv index
+            // uvIndexEl.textContent = ('UV Index: ' + data.)
         });
-})
+}
+mainWeatherData();
+
+// TODO: just create a for-loop nextime...
+function cardWeatherData() {
+
+    fetch(fiveDayURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var tempOne = document.querySelector('.card-temp-one');
+            var tempTwo = document.querySelector('.card-temp-two');
+            var tempThree = document.querySelector('.card-temp-three');
+            var tempFour = document.querySelector('.card-temp-four');
+            var tempFive = document.querySelector('.card-temp-five');
+            tempOne.textContent = ('Temp: ' + data.list[0].main.temp + '°F');
+            tempTwo.textContent = ('Temp: ' + data.list[4].main.temp + '°F');
+            tempThree.textContent = ('Temp: ' + data.list[9].main.temp + '°F');
+            tempFour.textContent = ('Temp: ' + data.list[14].main.temp + '°F');
+            tempFive.textContent = ('Temp: ' + data.list[19].main.temp + '°F');
+            var humidityOne = document.querySelector('.card-humidity-one');
+            var humidityTwo = document.querySelector('.card-humidity-two');
+            var humidityThree = document.querySelector('.card-humidity-three');
+            var humidityFour = document.querySelector('.card-humidity-four');
+            var humidityFive = document.querySelector('.card-humidity-five');
+            humidityOne.textContent = ('Humidity: ' + data.list[0].main.humidity + '%');
+            humidityTwo.textContent = ('Humidity: ' + data.list[4].main.humidity + '%');
+            humidityThree.textContent = ('Humidity: ' + data.list[9].main.humidity + '%');
+            humidityFour.textContent = ('Humidity: ' + data.list[14].main.humidity + '%');
+            humidityFive.textContent = ('Humidity: ' + data.list[19].main.humidity + '%');
+
+        });
+}
+cardWeatherData();
+
+// create a for loop for the moment.js dates
+// if uv-index > 4 add class red type of things
+function cardDates() {
+
+    var oneDate = moment().add(1, 'days').format('MM/DD/YY');
+    var twoDate = moment().add(2, 'days').format('MM/DD/YY');
+    var threeDate = moment().add(3, 'days').format('MM/DD/YY');
+    var fourDate = moment().add(4, 'days').format('MM/DD/YY');
+    var fiveDate = moment().add(5, 'days').format('MM/DD/YY');
+    var firstDate = document.querySelector('.one-date');
+    firstDate.textContent = (oneDate);
+    var secondDate = document.querySelector('.two-date');
+    secondDate.textContent = (twoDate);
+    var thirdDate = document.querySelector('.three-date');
+    thirdDate.textContent = (threeDate);
+    var fourthDate = document.querySelector('.four-date');
+    fourthDate.textContent = (fourDate);
+    var fifthDate = document.querySelector('.five-date');
+    fifthDate.textContent = (fiveDate);
+}
+cardDates();
+
+
+
+
+// function search() {
+//     this.mainWeatherData(document.querySelector('.input-field').value);
+// }
+
+// document.querySelector('btn').addEventListener('click', function() {
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // TODO: WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -56,15 +170,18 @@ searchButton.addEventListener('click', function () {
 // TODO: figure out how to display the rest of the data that's contained inside of weatherEl
 
 
+// // 5 day forcast url
+// var fiveDayForcast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + appKey;
+// console.log(fiveDayForcast);
 
 // example given in class
 // fetch(cityURL)
 //     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//         var weatherEl = document.querySelector('#weather');
-//         console.log(weather);
-//         weatherEl.textContent = parseInt(data.main.temp);
+//             return response.json();
+//         })
+//         .then(function (data) {
+//                 console.log(data);
+//                 var weatherEl = document.querySelector('#weather');
+//                 console.log(weather);
+//                 weatherEl.textContent = parseInt(data.main.temp);
 //     });
